0fa7b4eacf3142f955c50279526f48df13a316de
avchougule <akshaychougule878@gmail.com>
2022-11-17 23:06:51 +0530

    First commit

---
+++ src/entity/StudentVO.java
@@ -0,0 +1,45 @@
+package entity;
+
+public class StudentVO {
+	
+	int rollno;
+	String fname;
+	String lname;
+	String city;
+	String address;
+	
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
---
+++ src/entity/TestHibernate.java
@@ -0,0 +1,57 @@
+package entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class TestHibernate {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		//1. Create Configuration Object
+		Configuration config = new Configuration().configure("entity/hibernate.cfg.xml");
+		/*
+		 * StandardServiceRegistry ssr = new
+		 * StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		 * Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+		 */
+		
+		
+		//2. Create Session Factory Object
+		SessionFactory sessionFactory = config.buildSessionFactory();
+		//SessionFactory sessionFactory = meta.getSessionFactoryBuilder().build();
+		
+		//3. Open the Session
+		Session session = sessionFactory.openSession();
+		
+		//4. Begin the Transaction
+		session.beginTransaction();
+		
+		//5. Create Object of Class to save
+		
+		StudentVO obj = new StudentVO();
+		obj.setRollno(4);
+		obj.setFname("Akshay");
+		obj.setLname("Chougule");
+		obj.setCity("Pune");
+		
+		//6. Save the object into DB
+		
+		Integer val= (Integer)session.save(obj);
+		
+		//7. Commit the transaction
+		session.getTransaction().commit();
+		
+		//8. Close the session
+		sessionFactory.close();
+		session.close();
+		
+		System.out.println("Saved Student Roll no is - "+val);
+		
+	}
+
+}
